0e497752e79dace5376d4651d5b76fb67b3c197b
beatrizferreira22 <biasou799@gmail.com>
2025-03-20 14:39:46 -0400

    Add files via upload

---
+++ ex3.py
@@ -0,0 +1,10 @@
+num1=int(input("insira o primeiro numero"))
+num2=int(input("insira o segundo numero"))
+soma=num1+num2
+subtracao=num1-num2
+divisao=num1/num2
+multiplicacao=num1*num2
+print(f"(o resultado da soma é {soma})")
+print(f"(o resultado da subtracao é {subtracao})")
+print(f"(o resultado da divisao é {divisao})")
+print(f"(o resultado da multiplicacao é {multiplicacao}")
